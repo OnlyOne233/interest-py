793a5254d5c440b291354f546b37c8cb9d1fa1b7
OnlyOne233 <90247996+OnlyOne233@users.noreply.github.com>
2021-09-13 11:39:48 +0200

    Update Only.py

--- Only.py
+++ Only.py
@@ -1,1 +1,5 @@
-
+Kapital = int(input("What is the kapital?: "))
+interestinpercentage = float(input("\nWhat is the interestrate in percentage?: "))
+Years = int(input("\nHow many years is the interestrate being used: "))
+sumafterinterest =float ("{0:.2f}".format( Kapital*pow((interestinpercentage/100)+1,Years)))
+print("\nThe sum after", Years, "years is", sumafterinterest)
