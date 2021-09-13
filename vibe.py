03a576ebc61cfeee97437a1fc9517cb8c853b7d1
chasvibe <viktor.berg@chasacademy.se>
2021-09-13 10:08:54 +0200

    second commit

--- vibe.py
+++ vibe.py
@@ -1,4 +1,4 @@
-# Chas Handle : Vibe
+## Chas Handle : Vibe
 print("Hello, my handle is vibe from the first two letters in my first and last name (VIktor BErg)")
 res = input("What's yours? ")
 print("Hey " + res + "! Nice to meet you!")
